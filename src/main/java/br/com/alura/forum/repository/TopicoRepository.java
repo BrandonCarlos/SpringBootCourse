db4ce633634d17677cb174bee34da78626150226
BrandonCarlos <brandon_marzo@hotmail.com>
2022-08-29 13:53:18 -0300

    using other name to declared and using code JPQL

--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -3,6 +3,8 @@ package br.com.alura.forum.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.alura.forum.modelo.Topico;
 
@@ -13,5 +15,12 @@ public interface TopicoRepository extends JpaRepository<Topico, Long> {//JpaRepo
 	//Agora este método findByCursoNome é novo então declaramos ele aqui, que recebe a QUERY(nomeCurso) = localhost:8080?nomecurso=Spring
 	List<Topico> findByCursoNome(String nomeCurso);//Automáticamente fará uma QUERY no BD, assim SELECT * FROM TOPICO WHERE nomeCurso = QUERY(passado pelo usuario na URL)
 	//e assim não precisaremos mais utilizar o JPQL
+	
+	//Se quisermos nos mesmos colocarmos outro nome no método, então vamos precisar escrever toda a QUERY em formato JPQL e ai atribuimos o parâmetro @Param vinculado com o :nomeCurso
+	@Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")//:nomeCurso <- vai vir lá da URL, ex: https://localhost:8080?cursoNome=Spring+Boot
+	List<Topico> carregarPorNomeDoCurso(@Param("nomeCurso") String nomeCurso);
+
+	
+	
 }
  
